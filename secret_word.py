713e2cb2dfe99b1bb9c69ae59cf4274c95cf59ff
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-06 19:54:00 -0400

    started with the control flow but stuck on what to do next

--- secret_word.py
+++ secret_word.py
@@ -5,23 +5,21 @@ with open ('/usr/share/dict/words') as infile:
     word = random.choice(infile).lower()
 # print(word)
 
-
 full_word = '_ ' * len(word)
 guessed_letters = []
 attempts = 10
 print('Guess the word!')
-print(full_word)
-print('\n')
+print(full_word, '\n')
 
 while attempts > 0:
-    guess = input('Enter a letter: ')
-    if len(guess) > 1:
+
+    guess = input('Enter a letter: ').lower()
+    # guessed_letters.append(guess)
+    # print(guessed_letters)
+    if len(guess) > 1: #checks to see if user is only inputting 1 letter
         print('Enter only 1 letter')
-        continue
-    elif not guess.isalpha():
+    elif not guess.isalpha(): #makes sure the user is not inputting anything other than a letter
         print('Enter a letter only')
-        continue
-    elif guess in guessed_letters:
+    elif guess in guessed_letters: #makes sure the same letter is not used twice
         print('Letter has already been used')
-        continue
-    
+    for letter in word:
