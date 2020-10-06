497d595b2c9a9dcfa459cf4916174dfcdeca603e
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-06 10:22:11 -0400

    completed assignment

--- number_guessing_game_b.py
+++ number_guessing_game_b.py
@@ -1,12 +1,12 @@
 import random
-random_number = random.randint(1,10)
+user_num = int(input('Enter a number between 1 - 10: '))
 computer_guess = random.randint(1,10)
 turns = 0
 
-while random_number != computer_guess:
+while user_num != computer_guess:
     turns += 1
     print(computer_guess)
     computer_guess = random.randint(1,10)
-    if random_number == computer_guess:
+    if user_num == computer_guess:
         print('It took', turns, 'turns')
         break
